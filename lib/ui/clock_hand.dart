99739c9840e4ddbb74d09f2953d467a1c7be96bc
Biplab Dutta <biplabdutta860@gmail.com>
2022-07-01 12:07:10 +0545

    feat: add stopwatch hand

---
+++ lib/ui/clock_hand.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class ClockHand extends StatelessWidget {
+  const ClockHand({
+    required this.rotationZAngle,
+    required this.handThickness,
+    required this.handLength,
+    required this.color,
+    Key? key,
+  }) : super(key: key);
+
+  final double rotationZAngle, handThickness, handLength;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Transform(
+      alignment: Alignment.topRight,
+      transform: Matrix4.identity()
+        ..translate(-handThickness / 2, 0, 0)
+        ..rotateZ(rotationZAngle),
+      child: Container(
+        height: handLength,
+        width: handThickness,
+        color: color,
+      ),
+    );
+  }
+}
